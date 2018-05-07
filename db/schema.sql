76a28054b0c181c475397f36e710c02164833012
SallyTemple <pfende@gmail.com>
2018-05-06 21:29:39 -0700

    update

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+
+
+CREATE TABLE burgers
+(
+	id int AUTO_INCREMENT NOT NULL ,
+	burger_name varchar(50) NOT NULL,
+	devoured BOOLEAN NOT NULL DEFAULT 0,
+	PRIMARY KEY (id)
+	
+);
+
---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Calaloo Burger");
+INSERT INTO burgers (burger_name) VALUES ("Steak Onion Burger");
+INSERT INTO burgers (burger_name) VALUES ("Jerk Chicken Burger");
