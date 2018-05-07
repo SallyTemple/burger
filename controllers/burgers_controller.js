df2e81b57c2dddf3159c050be418d4e1cf9cd5bd
SallyTemple <pfende@gmail.com>
2018-05-06 23:51:15 -0700

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,20 +3,20 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   cat.create([
     "name", "sleepy"
   ], [
